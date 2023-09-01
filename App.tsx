bdb64dbd455cd798acafade7bdbd30784387cf7e
Caio18-cosenza <caiiocosenza2020@gmail.com>
2023-09-01 09:43:46 -0300

    authentication done

--- App.tsx
+++ App.tsx
@@ -1,11 +1,64 @@
+import * as LocalAuthentication from 'expo-local-authentication';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+
+const SECRET = [
+  'Icode-0ebd2062f-Mobile',
+  'caio-2dcc4ce1-cosenza',
+  'Macarrao-b6df-spaguett',
+  'feijao-d72fa9-arroz',
+  'secreto-0c0840-segredo',
+];
 
 export default function App() {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [password, setPassword] = useState<string>('Sua senha aqui');
+
+  function chosenPassword() {
+    return SECRET[Math.floor(Math.random() * SECRET.length)];
+  }
+
+  const handleAcessSecretPass = async () => {
+    const response = await LocalAuthentication.getEnrolledLevelAsync();
+    if (response === LocalAuthentication.SecurityLevel.BIOMETRIC) {
+      const result = await LocalAuthentication.authenticateAsync({
+        cancelLabel: 'Cancelar autenticação',
+        requireConfirmation: true,
+      });
+      if (result.success) {
+        Alert.alert('SUCESSO!', 'A sua senha secreta está disponível');
+        setShowPassword(true);
+        setPassword(chosenPassword);
+        setTimeout(() => {
+          setShowPassword(false);
+          setPassword('');
+        }, 5000);
+      } else {
+        Alert.alert('ERRO', 'Não foi possível logar!');
+      }
+    } else {
+      Alert.alert('SEM BIOMETRIA', 'Login sem biometria');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Tela de bloqueio baseado em telas sensíveis</Text>
+      <Button title='Acessar senha secreta' onPress={handleAcessSecretPass} />
+      {showPassword && (
+        <Text
+          style={{
+            color: '#ff0000',
+            textDecorationLine: 'underline',
+            fontWeight: 'bold',
+            marginTop: 5,
+          }}
+        >
+          {password}
+        </Text>
+      )}
+      <StatusBar style='auto' />
     </View>
   );
 }
